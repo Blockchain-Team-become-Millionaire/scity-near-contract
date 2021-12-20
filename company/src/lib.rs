23a3a729ba6f297d4bdb1ec2759de787ce324d36
Thanh Loc <locdt.developer@gmail.com>
2021-12-20 16:25:04 +0700

    cross-contract call transfer box to owner before open_box

--- company/src/lib.rs
+++ company/src/lib.rs
@@ -7,7 +7,8 @@ use near_sdk::collections::{LazyOption, LookupMap, UnorderedMap, UnorderedSet};
 use near_sdk::json_types::{Base64VecU8, ValidAccountId, U128, U64};
 use near_sdk::serde::{Deserialize, Serialize};
 use near_sdk::{
-    env, near_bindgen, AccountId, Balance, CryptoHash, PanicOnDefault, Promise, StorageUsage,
+    env, ext_contract, near_bindgen, AccountId, Balance, CryptoHash, Gas, PanicOnDefault, Promise,
+    StorageUsage,
 };
 
 pub use crate::enumerable::*;
@@ -25,6 +26,8 @@ mod nft_core;
 mod token;
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
+const PREPARE_GAS: Gas = 1_500_000_000_000_0;
+const BOX_CONTRACT: &str = "box.nft-test.testnet";
 
 near_sdk::setup_alloc!();
 
@@ -141,11 +144,19 @@ impl Contract {
 
         self.total = self.total + 1;
 
+        ext_box::transfer_box_to_owner(
+            BOX_CONTRACT.to_string(),
+            String::from("1"),
+            Some(String::from("Openbox")),
+            &"ft.nft-test.testnet",
+            1,
+            PREPARE_GAS,
+        );
         self.nft_mint(
             Some(token_id),
             token,
             Some(ValidAccountId::try_from(env::predecessor_account_id()).unwrap()),
-        )
+        );
     }
 
     pub fn get_companies_by_owner(&self, owner_id: AccountId) -> Vec<TokenMetadata> {
@@ -158,4 +169,15 @@ impl Contract {
             .map(|token_id| self.token_metadata_by_id.get(&token_id).unwrap())
             .collect()
     }
+}
+
+#[ext_contract(ext_box)]
+trait BoxContract {
+    fn transfer_box_to_owner(
+        &mut self,
+        receiver_id: AccountId,
+        amount: String,
+        memo: Option<String>,
+    );
+    fn ft_balance_of(&self, account_id: AccountId) -> U128;
 }
